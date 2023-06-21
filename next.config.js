c1f20d2cf9e3eb173eae1aa6adc2b233c64be3c6
Sana Parveen <misana348@gmail.com>
2023-06-21 10:48:26 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['www.pexels.com', 'images.pexels.com'],
+  },
+  images:{
+    remotePatterns:[
+     {
+         protocol:"https",
+         hostname:"*"
+     }
+    ]
+ }
+};
+
+module.exports = nextConfig;
